d64b9c22ebc4771bbb85d5f793f383f9b93bb446
Zareeb_Chowdhury <38160697+ZareebC@users.noreply.github.com>
2019-08-05 18:05:25 -0400

    Update variables.cpp

--- variables.cpp
+++ variables.cpp
@@ -1,13 +1,66 @@
 #include "variables.h"
 #include "main.h"
 #include <math.h>
+
 pros::Motor move1 (1);
 pros::Motor move2 (2);
 pros::Motor move3 (3);
 pros::Motor move4 (4);
-void drive(int ch1, int ch2, int ch3, int ch4){ //a corresponds to the individual analog button axes
-  move4.move(ch3  + ch4 + ch1);
+pros::Motor twoBar (5);
+pros::Motor leftRoller (6);
+pros::Motor rightRoller (7);
+pros::Motor pusher (8);
+
+
+void drive(int ch1, int ch2, int ch3, int ch4)
+{
   move1.move(ch3 - ch4 + ch1);
   move2.move(-(ch3 - ch4 - ch1));
   move3.move(-(ch3 + ch4 - ch2));
+  move4.move(ch3  + ch4 + ch1);
+}
+
+void lift(bool liftUpButton, bool liftDownButton)
+{
+  if(liftUpButton)
+  {
+    twoBar.move_velocity(100);
+  }
+  else if(liftDownButton)
+  {
+    twoBar.move_velocity(-100);
+  }
+  else{
+    twoBar.move_velocity(0);
+  }
+}
+
+void roll(bool rollUpButton, bool rollDownButton)
+{
+  if(rollUpButton)
+  {
+    leftRoller.move_velocity(100);
+    rightRoller.move_velocity(-100);
+  }
+  else if(rollDownButton)
+  {
+    leftRoller.move_velocity(-100);
+    rightRoller.move_velocity(100);
+  }
+  else
+  {
+    leftRoller.move_velocity(0);
+    rightRoller.move_velocity(0);
+  }
+}
+void push(bool pushForwardButton, bool pullBackwardsButton){
+  if(pushForwardButton){
+    pusher.move_velocity(20);
+  }
+  else if(pullBackwardsButton){
+    pusher.move_velocity(-20);
+  }
+  else{
+    pusher.move_velocity(0);
+  }
 }
