ec2db0477f3c4860a8ba01c0124b581e22591d61
Zareeb_Chowdhury <38160697+ZareebC@users.noreply.github.com>
2019-09-30 15:09:03 -0400

    commit

--- opcontrol.cpp
+++ opcontrol.cpp
@@ -2,16 +2,6 @@
 #include "variables.h"
 //using namespace okapi;
 
-int FRONT_LEFT = 4;
-int FRONT_RIGHT = 2;
-int BACK_LEFT = 1;
-int BACK_RIGHT = -3;
-
-int TWO_BAR = 5;
-int RIGHT_ROLLER = 6;
-int LEFT_ROLLER = 7;
-int PUSHER = 8;
-
 pros::Motor FrontRight (FRONT_RIGHT);
 pros::Motor FrontLeft (FRONT_LEFT);
 pros::Motor BackLeft (BACK_LEFT);
@@ -21,63 +11,80 @@ pros::Motor RightRoller (RIGHT_ROLLER);
 pros::Motor LeftRoller (LEFT_ROLLER);
 pros::Motor Pusher (PUSHER);
 
+
 int RightX;
 int RightY;
 int LeftY;
 int LeftX;
+
+bool rollUpButton;
+bool rollDownButton;
 bool liftUpButton;
 bool liftDownButton;
 bool pushForwardButton;
 bool pullBackwardsButton;
-int thresh = 20;
+bool slowrollfor;
+bool slowrollback;
+int thresh = 40;
 
-void opcontrol() {
+void opcontrol()
+{
 
 	pros::Controller controller (CONTROLLER_MASTER);
 
-	while(true){
+	while(true)
+	{
 
     RightX = controller.get_analog(ANALOG_RIGHT_X);
 		RightY = controller.get_analog(ANALOG_RIGHT_Y);
-		LeftY = controller.get_analog(ANALOG_LEFT_Y);
+		LeftY = -(controller.get_analog(ANALOG_LEFT_Y));
 		LeftX = controller.get_analog(ANALOG_LEFT_X);
 
-		bool rollUpButton = controller.get_digital(DIGITAL_R1);
-		bool rollDownButton = controller.get_digital(DIGITAL_R2);
-    bool liftUpButton = controller.get_digital(DIGITAL_L1);
-    bool liftDownButton = controller.get_digital(DIGITAL_L2);
-		bool pushForwardButton = controller.get_digital(DIGITAL_UP);
-		bool pullBackwardsButton = controller.get_digital(DIGITAL_DOWN);
+		rollUpButton = controller.get_digital(DIGITAL_R2);
+		rollDownButton = controller.get_digital(DIGITAL_R1);
+    liftUpButton = controller.get_digital(DIGITAL_L2);
+    liftDownButton = controller.get_digital(DIGITAL_L1);
+		pushForwardButton = controller.get_digital(DIGITAL_UP);
+		pullBackwardsButton = controller.get_digital(DIGITAL_DOWN);
+		slowrollfor = controller.get_digital(DIGITAL_A);
+		slowrollback = controller.get_digital(DIGITAL_Y);
 
-		if((abs(RightX) > 0) || (abs(RightY) > 0) || (abs(LeftY) > 0) || (abs(LeftX) > 0)){
-			if(abs(RightX) < thresh) {
+		if((abs(RightX) > 0) || (abs(RightY) > 0) || (abs(LeftY) > 0) || (abs(LeftX) > 0))
+		{
+			if(abs(RightX) < thresh)
+			{
 				RightX = 0;
 			}
-			if(abs(RightY) < thresh) {
+			if(abs(RightY) < thresh)
+			{
 				RightY = 0;
 			}
-			if(abs(LeftY) < thresh) {
+			if(abs(LeftY) < thresh)
+			{
 				LeftY = 0;
 			}
-			if(abs(LeftX) < thresh) {
+			if(abs(LeftX) < thresh)
+			{
 				LeftX = 0;
 			}
 
-			if(abs(LeftY) > abs(RightY)) {
+			if(abs(LeftY) > abs(RightY))
+			{
 				RightY = 0;
 			}
 			else {
 				LeftY = 0;
 			}
 
-			if(abs(LeftY) > 80){
+			if(abs(LeftY) > 80)
+			{
 				RightX *= .707106781186548;
 			}
 		}
 
-		drive(RightX, RightY, LeftY, LeftX);
+		drive(RightX, RightY, LeftX, LeftY);
     lift(liftUpButton, liftDownButton);
-		roll(rollUpButton, rollDownButton);
+		roll(rollUpButton, rollDownButton, slowrollfor, slowrollback);
 		push(pushForwardButton, pullBackwardsButton);
 
 		pros::Task::delay(10);
